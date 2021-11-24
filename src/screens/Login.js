e84e128661ad395c4317fa9400ac450c8e476775
Santiagolemoine <slemoine@udesa.edu.ar>
2021-11-24 16:23:48 -0300

    CSS de login y register listo

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -19,7 +19,9 @@ class Login extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
+      <View >
+        <View style={styles.container}> 
+        <Text style={styles.logo} >PhotoFactory </Text>
         <TextInput placeholder='email'
                     keyboardType= 'email-address'
                     style={styles.input}
@@ -29,22 +31,26 @@ class Login extends Component {
                     secureTextEntry={true}
                     style={styles.input}
                     onChangeText={text => this.setState({pass: text})}/>
-        <TouchableOpacity 
-                        onPress={()=> this.navigateToRegister()}>
-                <Text >Todavia no tenes una cuenta? Registrate</Text>
-        </TouchableOpacity>
+        
         <Text>{this.props.errorLogin}</Text>
         {console.log(this.props.errorLogin)}
         {this.state.email && this.state.pass ?
           <>
           <TouchableOpacity style={styles.touchable}
           onPress={()=> this.props.login(this.state.email, this.state.pass)}>
-          <Text style={styles.touchableText} >Login</Text>
+          <Text style={styles.touchableText} >Log in</Text>
           </TouchableOpacity> 
           </>
           :
         <Text></Text>
       }
+      </View> 
+      <View style={styles.container2}>
+      <TouchableOpacity 
+                        onPress={()=> this.navigateToRegister()}>
+                <Text >Todavia no tenes una cuenta? <Text style={styles.blue}>Registrate</Text ></Text>
+        </TouchableOpacity>
+      </View> 
       </View>
     );
   }
@@ -53,19 +59,49 @@ class Login extends Component {
 const styles = StyleSheet.create({
   container:{
       paddingHorizontal: 10,
-      marginVertical: 20
-
+      marginVertical: 20,
+      alignItems: 'center',
+      borderRadius: 1,
+      borderWidth: 1,
+      borderStyle: 'solid',
+      borderColor: "#BFBFBF ",
+      marginTop: 50,
+      marginHorizontal: 30,
+      backgroundColor: "white"
+  },
+  container2:{
+    paddingHorizontal: 10,
+    marginVertical: 20,
+    alignItems: 'center',
+    borderRadius: 1,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: "#BFBFBF ",
+    marginTop: 10,
+    marginHorizontal: 30,
+    backgroundColor: "white",
+    paddingVertical: 20,
+  },  
+  
+  logo:{
+      fontSize: 50,
+      fontFamily: "Comic Sans MS",
+      paddingBottom: 49,
+      paddingTop: 39,
   },
   touchable:{
-      backgroundColor: '#28a745',
+      backgroundColor: "#3097FF",
       paddingHorizontal: 10,
       paddingVertical: 6,
       textAlign: 'center',
       borderRadius: 4,
       borderWidth: 1,
       borderStyle: 'solid',
-      borderColor: '#28a745'
-
+      borderColor: "#3097FF",
+      with: 80,
+      paddingHorizontal: 40,
+      marginBottom: 25,
+      marginTop: 17,
   },
   touchableText:{
       color: '#fff'
@@ -76,9 +112,12 @@ const styles = StyleSheet.create({
       borderWidth: 1,
       borderColor: '#ccc',
       borderRadius: 6,
-      paddingHorizontal: 10,
+      paddingHorizontal: 25,
       paddingVertical: 15,
-      marginVertical: 10
+      marginVertical: 14
+  },
+  blue:{
+    color: "#0804FC"
   }
 });
 
--- src/screens/Register.js
+++ src/screens/Register.js
@@ -21,16 +21,20 @@ class Register extends Component {
   render() {
     return (
       <View>
+      
+      <View style={styles.container}> 
+        <Text style={styles.logo}>PhotoFactory </Text>
       <TextInput
         onChangeText={(text) => this.setState({ userName: text })}
         placeholder="user name"
         keyboardType="default"
-        
+        style={styles.input}
       />
       <TextInput
         onChangeText={(text) => this.setState({ email: text })}
         placeholder="email"
         keyboardType="email-address"
+        style={styles.input}
       />
       
       <TextInput
@@ -38,42 +42,96 @@ class Register extends Component {
         placeholder="password"
         keyboardType="email-address"
         secureTextEntry={true}
+        style={styles.input}
       />
-      <TouchableOpacity
-        onPress={() => this.navigateToLogin()}
-      >
-        <Text >Ya tenes una cuenta? Ingresa aca </Text>
-      </TouchableOpacity>
+      
       <Text > {this.props.errorRegister} </Text>
 
       {this.state.email && this.state.password && this.state.userName ? 
         <TouchableOpacity
-        style={styles.button}
+        style={styles.touchable}
         onPress={() => this.props.register(this.state.email, this.state.password, this.state.userName)}
         >
-          <Text style={styles.textButton}>Registrar</Text>
+          <Text style={styles.touchableText}>Registrar</Text>
         </TouchableOpacity> :
         <Text></Text>
       }
+      </View>
+      <View style={styles.container2}>
+      <TouchableOpacity
+        onPress={() => this.navigateToLogin()}
+      >
+        <Text >Ya tenes una cuenta? <Text style={styles.blue}>Ingresa aca </Text ></Text>
+      </TouchableOpacity>
+      </View>
     </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  button: {
-    backgroundColor: "#28a745",
+  container:{
+    paddingHorizontal: 10,
+    marginVertical: 20,
+    alignItems: 'center',
+    borderRadius: 1,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: "#BFBFBF ",
+    marginTop: 50,
+    marginHorizontal: 30,
+    backgroundColor: "white"
+},
+container2:{
+  paddingHorizontal: 10,
+  marginVertical: 20,
+  alignItems: 'center',
+  borderRadius: 1,
+  borderWidth: 1,
+  borderStyle: 'solid',
+  borderColor: "#BFBFBF ",
+  marginTop: 10,
+  marginHorizontal: 30,
+  backgroundColor: "white",
+  paddingVertical: 20,
+},  
+
+logo:{
+    fontSize: 50,
+    fontFamily: "Comic Sans MS",
+    paddingBottom: 49,
+    paddingTop: 39,
+},
+touchable:{
+    backgroundColor: "#3097FF",
     paddingHorizontal: 10,
     paddingVertical: 6,
-    textAlign: "center",
+    textAlign: 'center',
     borderRadius: 4,
     borderWidth: 1,
-    borderStyle: "solid",
-    borderColor: "#28a745",
-  },
-  textButton: {
-    color: "#fff",
-  },
+    borderStyle: 'solid',
+    borderColor: "#3097FF",
+    with: 80,
+    paddingHorizontal: 40,
+    marginBottom: 25,
+    marginTop: 17,
+},
+touchableText:{
+    color: '#fff'
+},
+input: {
+    height: 20,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 6,
+    paddingHorizontal: 25,
+    paddingVertical: 15,
+    marginVertical: 14
+},
+blue:{
+  color: "#0804FC"
+}
 });
 
 export default Register;
