123a07473fe24b4f754d0d178462c56d008bde05
Santiagolemoine <slemoine@udesa.edu.ar>
2021-11-24 20:38:09 -0300

    new post estilo y css terminado

--- src/screens/NewPost.js
+++ src/screens/NewPost.js
@@ -52,16 +52,25 @@ class NewPost extends Component{
     render(){
         return(
             this.state.showCamera ? <MyCamera onImageUpload={(url)=> this.onImageUpload(url)}/> :
+            <View state={styles.granContenedor}>
             <View style={styles.formContainer}>
-                <TextInput
+                <Text>
+                    {auth.currentUser.displayName}:
+                    <TextInput
                     style={styles.input}
                     placeholder='Título'
                     keyboardType= 'default'
                     onChangeText = {text => this.setState({title: text})}
                     value={this.state.title}
-                ></TextInput>
+                ></TextInput> 
+                </Text>
+                <Image 
+                    style= {styles.img} 
+                    source= {require("../../assets/Fotografías.webp")}
+                    resizeMode= "cover"
+                 />
                 <TextInput
-                    style={styles.input}
+                    style={styles.input2}
                     placeholder='Descripción'
                     keyboardType= 'default'
                     onChangeText = {text => this.setState({description: text})}
@@ -74,47 +83,79 @@ class NewPost extends Component{
                     <Text>Postear</Text>
                 </TouchableOpacity>
             </View>
+            </View>
         )
     }
 }
 const styles = StyleSheet.create({
+
+    granContenedor:{
+        alignItems: 'center',
+        backgroundColor: 'white',
+        height: '100%',
+        flex: 1,
+        flexDirection: "row",
+    },
     formContainer:{
-        paddingHorizontal:10,
-        marginTop: 20,
+        paddingHorizontal: 10,
+        borderRadius: '1%',
+        marginVertical: 20,
+        padding: '5%',
+
+        
     },
     multilineInput:{
         height:100,
         paddingVertical:15,
         paddingHorizontal: 10,
         borderWidth:1,
-        borderColor: '#ccc',
+        borderColor: "#3097FF",
         borderStyle: 'solid',
         borderRadius: 6,
         marginVertical:10,
     },
     button:{
-        backgroundColor:'#28a745',
+        backgroundColor:"#3097FF",
         paddingHorizontal: 10,
         paddingVertical: 6,
         textAlign: 'center',
         borderRadius:4, 
         borderWidth:1,
         borderStyle: 'solid',
-        borderColor: '#28a745'
+        borderColor: "#3097FF"
     },
     textButton:{
         color: '#fff'
     },
+    img: {
+        height: 200,
+        borderRadius: 2,
+        resizeMode : "cover",
+        marginBottom: 7,
+        marginTop: 8,
+      },
 
     input: {
         height: 20,
         borderStyle: 'solid',
         borderWidth: 1,
-        borderColor: '#ccc',
+        borderColor: "#3097FF",
         borderRadius: 6,
         paddingHorizontal: 10,
         paddingVertical: 15,
+        marginVertical: 10,
+        marginLeft: 15
+    },
+    input2: {
+        height: 20,
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderColor: "#3097FF",
+        borderRadius: 6,
+        paddingHorizontal: 10,
+        paddingTop: 15,
+        paddingBottom: 32,
         marginVertical: 10
-    }
+    },
 })
 export default NewPost;
